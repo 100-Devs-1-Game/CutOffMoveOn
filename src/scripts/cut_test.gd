becd5ca361fdb64262914f2bd34d810013c59c3e
Joseph Schiller <spacesorcererstudio@gmail.com>
2026-01-11 15:36:01 -0600

    adding taping stage

--- src/scripts/cut_test.gd
+++ src/scripts/cut_test.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
-const PIECE3_END_POS := Vector2(-340, 0)
+const CUT_STAGE := 0
+const TAPE_STAGE := 1
+
+const PIECE3_END_POS := Vector2(-338, 0)
 const MIN_POINT_DIST := 48
 const POINT_DIST_SQR := MIN_POINT_DIST * MIN_POINT_DIST
-const cut_texture: Texture2D = preload("res://dotted_line.png")
+const cut_texture: Texture2D = preload("res://gfx/dotted_line.png")
 const PIECE2_END_POS := Vector2(1536.0, 256)
 
 @export var _guide_lines: Array[Line2D] = []
-
+@onready var _tape_guide: Line2D = $TapeLine
 
 #@onready var _piece1: Sprite2D = $Piece1
 @onready var _piece2: Sprite2D = $Piece2
@@ -16,62 +19,82 @@ const PIECE2_END_POS := Vector2(1536.0, 256)
 @onready var _cursor: Node2D = $Cursor
 
 
-var _point_dict: Dictionary = {}
+var _cut_points: Array[Vector2i] = []
+var _tape_points: Array[Vector2i] = []
 var _spawned_lines: Array[Line2D] = []
 
+var current_stage := CUT_STAGE
 var _current_line: Line2D
-var is_cutting: bool = false
+var using_tool: bool = false
 var tick_delay = 10
 var tick = 0
+
 
 func _ready() -> void:
 	_final.modulate = Color.TRANSPARENT
 	for line in _guide_lines:
 		for i in range(line.get_point_count()):
 			var pos = Vector2i(line.get_point_position(i))
-			_point_dict[pos] = false
+			_cut_points.append(pos)
+	for i in range(_tape_guide.get_point_count()):
+		var pos = Vector2i(_tape_guide.get_point_position(i))
+		_tape_points.append(pos)
 
 
 func _process(_delta: float) -> void:
 	_cursor.position = get_global_mouse_position()
-	if Input.is_action_just_pressed("click"): _begin_cut()
-	elif Input.is_action_just_released("click"): _end_cut()
-	elif Input.is_action_pressed("click"): _continue_cut()
+	if Input.is_action_just_pressed("click"): _begin_tool()
+	elif Input.is_action_just_released("click"): _end_tool()
+	elif Input.is_action_pressed("click"): _continue_tool()
 
 
-func _begin_cut() -> void:
-	is_cutting = true
-	_current_line = spawn_line()
+func _begin_tool() -> void:
+	using_tool = true
+	if current_stage == CUT_STAGE:
+		_current_line = spawn_line()
 
 
-func _end_cut() -> void:
-	if not is_cutting: return
-	_current_line = null
-	is_cutting = false
-	#TODO: Cleanup Lines, merge if possible
+func _end_tool() -> void:
+	if not using_tool: return
+	using_tool = false
+	if current_stage == CUT_STAGE:
+		_current_line = null
+	#TODO: Cleanup Lines, merge if possible, check for empty
 
 
-func _continue_cut() -> void:
-	if not is_cutting: return
+func _continue_tool() -> void:
+	if not using_tool: return
+	if current_stage == CUT_STAGE: _handle_cutting()
+	else: _handle_taping()
+
+
+func _handle_cutting() -> void:
 	tick -= 1
 	if tick > 0: return
 	tick = tick_delay
-	var nearest: Vector2i = get_nearest_point()
+	var nearest: Vector2i = get_nearest_point(_cut_points)
 	if nearest == -Vector2i.ONE: return
-	_point_dict.erase(nearest)
+	_cut_points.remove_at(_cut_points.find(nearest))
 	_current_line.add_point(nearest)
-	if is_complete(): on_cut_complete()
+	# There is occassionally(?) a stray point that is missed
+	if _cut_points.size() <= 1: on_cut_complete()
 
 
-func get_nearest_point() -> Vector2i:
-	for point in _point_dict.keys():
+func _handle_taping() -> void:
+	tick -= 1
+	if tick > 0: return
+	tick = tick_delay
+	var nearest: Vector2i = get_nearest_point(_tape_points)
+	if nearest == -Vector2i.ONE: return
+	_tape_points.remove_at(_tape_points.find(nearest))
+	if _tape_points.size() == 0: on_level_complete()
+
+
+func get_nearest_point(arr: Array[Vector2i]) -> Vector2i:
+	for point in arr:
 		if get_global_mouse_position().distance_squared_to(point) < POINT_DIST_SQR:
 			return point
 	return -Vector2i.ONE
-
-
-func is_complete() -> bool:
-	return _point_dict.size() == 0
 
 
 func spawn_line() -> Line2D:
@@ -99,6 +122,15 @@ func on_cut_complete() -> void:
 		create_tween().tween_property(
 			line, "modulate", Color.TRANSPARENT, 0.3
 		)
+	current_stage = TAPE_STAGE
+	$Cursor/Sprite2D.texture = load("res://gfx/tape.png")
+	$Cursor/Sprite2D.rotation = 0
+	$TapeStrip.visible = true
+
+
+
+func on_level_complete() -> void:
+	$TapeStrip.visible = false
 	await create_tween().tween_property(
 		_final, "modulate", Color(10, 10, 10, 10), 0.3).finished
 	await create_tween().tween_property(
